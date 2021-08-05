92d2680222c3e533c74c9d4c17e74a44b1ce66aa
jsetzer <joachim.setzer@t-systems.com>
2021-08-05 10:23:54 +0200

    files where renamed

--- 01_resource-group.tf
+++ 01_resource-group.tf
@@ -1,11 +1,9 @@
 resource "azurerm_resource_group" "myTerraformGroup" {
 #  name     = "myTerrafromGroup"
 #  location = "West Europe"
+#  mooved to vseperate files
   name = var.resourceGroupName
   location = var.location
   tags = var.tags
 }
 
-output "aaa" {
-  value = azurerm_network_security_group.myTerraformNsg.name
-}
